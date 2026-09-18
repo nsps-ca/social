8c460359e375192c1291fd5177f2e0a42ad1e8ee
Andy McKay <74699+andymckay@users.noreply.github.com>
2026-09-18 14:35:52 -0700

    reset, send to #chat and have nicer message

--- web.py
+++ web.py
@@ -7,19 +7,41 @@ from slack_sdk.errors import SlackApiError
 
 slack_token = os.environ.get("SLACK_BOT_TOKEN", None)
 client = WebClient(token=slack_token)
-channel = "#it-strategy"
+channel = "#chat"
 
-def send_to_slack(msg):
+
+def send_to_slack(image):
     if not slack_token:
-        print("❌ SLACK_BOT_TOKEN environment variable not set, not sending to Slack but would have sent the following to {channel}")
+        print(
+            "❌ SLACK_BOT_TOKEN environment variable not set, not sending to Slack but would have sent the following to {channel}"
+        )
         print(msg)
         return
 
+    summary = f"📣 Image has changed on the website to *{image['what']}* by *{image['who']}*"
+    social = "⛔️ This image is *NOT* approved for posting on social media, so please don't do that."
+    if social:
+        social = "✅ This image is approved to share on NSPS social media accounts."
+    blocks = [
+        {"type": "section", "text": {"type": "mrkdwn", "text": summary}},
+        {"type": "section", "text": {"type": "mrkdwn", "text": social}},
+        {"type": "divider"},
+        {
+            "type": "image",
+            "image_url": image["src"],
+            "title": {
+                "type": "plain_text",
+                "text": f"{image['what']} by {image['who']}"
+            },
+            "alt_text": f"{image['what']} by {image['who']}"
+        },
+    ]
     client.api_test()
     try:
-        client.chat_postMessage(channel=channel, text=msg)
+        client.chat_postMessage(channel=channel, text=summary, blocks=blocks)
     except SlackApiError as e:
         print(f"❌ Error sending message to Slack: {e.response['error']}")
+
 
 def check_website():
     old_image = ""
@@ -28,28 +50,27 @@ def check_website():
 
     res = requests.get("https://nsps.ca", headers={"User-Agent": "mod_security sucks"})
     res.raise_for_status()
-    soup = BeautifulSoup(res.content, 'html.parser')
-    hero = soup.find("figure").find("img")
-    src = hero["src"]
-    who = soup.find("figure").find("figcaption").next_element.strip()
-    what = soup.find("figure").find("figcaption").next_element.next_element.text.strip()
-    social_media = True if "SOCYes" in src else False
+    soup = BeautifulSoup(res.content, "html.parser")
 
+    figure = soup.find("figure")
+    caption = figure.find("figcaption")
+    src = figure.find("img")["src"]
+
+    data = {
+        "src": src,
+        "who": caption.next_element.strip(),
+        "what": caption.next_element.next_element.text.strip(),
+        "social": True if "SOCYes" in src else False,
+    }
     if src == old_image:
         print("👍 Image hasn't changed.")
-        
-    elif not social_media:
-        print("👍 Image has changed, but social media is set to `No`, so don't post.")
-        msg = f"📣 The image on the website changed to {src} by **{who}**, titled **{what}**. However is *NOT* approved to post on social media, so please don't do that."
-        send_to_slack(msg)
 
-    elif social_media:
-        print("✅ Image has changed, and social media is set to `Yes`, so let's share it.")
-        msg = f"📣 The image on the website changed to {src} by **{who}**, titled **{what}** and has approval 👍 to post on social media ."
-        send_to_slack(msg)
+    else:
+        send_to_slack(data)
 
     with open(".old-image", "w") as f:
         f.write(src)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     check_website()
